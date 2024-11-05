57f4332fe1d70cf7f61e46f3461bdc2e6696c8c2
Martin <soy26@bu.edu>
2024-11-06 05:49:02 +0800

    Added a basic footer
    Added a footer, linking will be added later

--- spark-bytes/app/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-import {  Navbar,   NavbarBrand,   NavbarContent,   NavbarItem,   NavbarMenuToggle,  NavbarMenu,  NavbarMenuItem} from "@nextui-org/navbar";
-
-export default function Header() {
-  return (
-    <Navbar>
-      <NavbarBrand>Spark Bytes</NavbarBrand>
-    </Navbar>
-  );
-}
