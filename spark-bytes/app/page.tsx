453671000a6ed2b5f61550f8a80a67f9af0e9fa7
Martin <soy26@bu.edu>
2024-11-06 05:51:52 +0800

    Update page.tsx

--- spark-bytes/app/page.tsx
+++ spark-bytes/app/page.tsx
@@ -2,6 +2,7 @@ import Header from "./components/header";
 import './globals.css';
 import { Image } from "@nextui-org/react";
 import { Nunito } from 'next/font/google';
+import Foot from "./components/Foot";
 
 const nunito = Nunito({
   subsets: ['latin'],
@@ -33,55 +34,7 @@ export default function Home() {
         </div>
       </div>
     </div>
-      
-      
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <Foot></Foot>      
     </>
   );
 }
