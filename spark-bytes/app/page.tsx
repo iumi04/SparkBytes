de8761ffacbc9ae87ebd9735d470e556adde043c
aidenwong04 <145720328+aidenwong04@users.noreply.github.com>
2024-11-05 16:25:29 -0500

    Implemented font styling and home page styling

--- spark-bytes/app/page.tsx
+++ spark-bytes/app/page.tsx
@@ -1,57 +1,40 @@
-import Image from "next/image";
 import Header from "./components/header";
 import './globals.css';
+import { Image } from "@nextui-org/react";
+import { Nunito } from 'next/font/google';
+
+const nunito = Nunito({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-background text-foreground">
+    <>
       <Header />
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="flex items-center justify-center min-h-screen p-8 bg-background text-foreground">
+      <div className="flex w-full max-w-4xl">
+        <div className="w-1/2">
+          <Image
+            className="dark:invert rounded-lg object-cover object-center"
+            src="/spark_bytes.jpeg"
+            alt="Spark Bytes logo"
+            width="100%"
+            height="auto"
+          />
         </div>
-      </main>
+        <div className="w-1/2 p-8 font-nunito">
+          <h1 className="text-4xl font-bold mb-4 mt-35">spark! bytes</h1>
+          <div className="p-0.5 rounded-lg bg-gradient-to-r from-white/10 to-gray-300">
+            <div className="text-lg bg-white rounded-lg shadow-lg backdrop-blur-md p-4">
+              Spark! Bytes is a revolutionary platform for Boston University’s community. After events, any leftover food can be posted here, and those interested can sign up to grab some delicious free food.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+      
+      
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -99,6 +82,6 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
-    </div>
+    </>
   );
 }
--- spark-bytes/tailwind.config.ts
+++ spark-bytes/tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
