71fbc389788aad1a993996621508b86ca0b6e35d
Martin <soy26@bu.edu>
2024-12-21 02:53:50 +0800

    Bug fix

--- spark-bytes/app/frontend/pages/events/page.tsx
+++ spark-bytes/app/frontend/pages/events/page.tsx
@@ -181,7 +181,7 @@ export default function Events() {
           ) : (
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center"
             style={{
-              rowGap: '0%', 
+              rowGap: '1%', 
               columnGap: '20%',
             }}>
               {currentEvents.map((event) => (
